e06205053cd65c5a66d797cee70b655839b94ca5
angel-arsov <angel.g.arsov@gmail.com>
2017-04-13 19:01:58 +0300

    fixes in home page + layout

--- controllers/home.js
+++ controllers/home.js
@@ -3,7 +3,7 @@ const Discussion = mongoose.model('Discussion');
 
 module.exports = {
     index: (req, res) => {
-        Discussion.find({}).limit(6).populate('author').then(discussions => {
+        Discussion.find({}).sort({date: -1}).limit(8).populate('author').then(discussions => {
             res.render('home/index', {discussions: discussions});
         });
     }
