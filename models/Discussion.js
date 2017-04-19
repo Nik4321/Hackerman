d904c84ddaf01f042064cfd7a7e10e6a65428734
Bobi2000 <bobko_2000@abv.bg>
2017-04-19 15:46:44 +0300

    At last the replying is saving in mongoose!

--- models/Discussion.js
+++ models/Discussion.js
@@ -10,7 +10,7 @@ let discussionSchema = mongoose.Schema({
 });
 
 let discussionReplyingSchema = mongoose.Schema({
-    content: {type: String, required: true},
+    content: {type: String},
     author: {type: mongoose.Schema.Types.ObjectId, required: true, ref: 'User'},
     date: {type: Date, default: Date.now()}
 });
